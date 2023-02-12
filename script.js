c545a851981337050fe1b1a4100bef5f6702f0c0
Oscar <oscar.jcarabali@gmail.com>
2023-02-12 11:03:09 -0500

    enlasando js y probando querySelector

---
+++ script.js
@@ -0,0 +1,13 @@
+const allH1 = document.querySelector('h1');
+const parrafo2 = document.querySelector('.parrafo2');
+const idparrafo3 = document.querySelector('#parrafo3');
+const input = document.querySelector('input');
+
+console.log({
+    allH1,
+    parrafo2,
+    idparrafo3,
+    input
+});
+
+console.log(input.value);
