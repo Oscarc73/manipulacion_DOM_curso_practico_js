88f93af51f1d368f5220e515b9570a647022269e
Oscar <oscar.jcarabali@gmail.com>
2023-02-12 20:01:16 -0500

    se crea el archivo .gitignore y se crea y/o modifica el html desde js usando get y set attribute, classList, createElement y append. todo desde el JS

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const allH1 = document.querySelector('h1');
 const parrafo2 = document.querySelector('.parrafo2');
 const idparrafo3 = document.querySelector('#parrafo3');
 const input = document.querySelector('input');
+const dblank = document.getElementById('blank'); // con getElementById no es necesario discriminar si es una clase . o un id #
+const imageDivBlank = document.createElement('img');
 
 console.log({
     allH1,
@@ -11,3 +13,24 @@ console.log({
 });
 
 console.log(input.value);
+
+console.log(idparrafo3.getAttribute('id'));
+
+parrafo2.setAttribute('class', 'pf2');
+parrafo2.setAttribute('id', 'parrafo2');
+
+console.log({
+    allH1,
+    parrafo2,
+    idparrafo3,
+    input
+});
+
+dblank.classList.add('parrafoblank')
+
+imageDivBlank.setAttribute('src', './images/american_football.png')
+
+console.log(imageDivBlank);
+
+dblank.append(imageDivBlank);
+
